140fcda345c41bdeccf772afa4db4f37988208ce
Phillip Henry III <philtrader71@gmail.com>
2019-12-28 20:47:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
 new_hash = {
   {:home => {
     :team_name => "", :colors => [], :players => [{}]
-    }, 
+    },
   :away => {
     :team_name => "", :colors => [], :players => [{}]
     }
